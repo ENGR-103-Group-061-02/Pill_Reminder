f36a135ff1aae13d4de37a179abc598fb6c6da35
Liston <bsl38@drexel.edu>
2016-04-19 09:39:27 -0400

    Finished Pill Class

--- app/src/main/java/com/drexel/engr103grp061_02/pillreminder/database/Pill.java
+++ app/src/main/java/com/drexel/engr103grp061_02/pillreminder/database/Pill.java
@@ -5,4 +5,51 @@ package com.drexel.engr103grp061_02.pillreminder.database;
  */
 public class Pill {
     //TODO Add required fields and methods
+    String name;
+    int quantity;
+    int hours;
+    int minutes;
+    String instrutctions;
+
+    //SETTER METHODS:
+    public void setName(String name)
+    {
+        this.name=name;
+    }
+    public void setQuantity(int quantity)
+    {
+        this.quantity=quantity;
+    }
+    public void setTime(int hours, int minutes)
+    {
+        this.hours=hours;
+        this.minutes=minutes;
+    }
+    public void setInstrutctions(String instrutctions)
+    {
+        this.instrutctions=instrutctions;
+    }
+    //GETTER METHODS
+    public String getName(){
+        return name;
+    }
+    public int getQuantity(){
+        return quantity;
+    }
+    public int getHours(){
+        return hours;
+    }
+    public int getMinutes(){
+        return minutes;
+    }
+    public String getInstrutctions(){
+        return instrutctions;
+    }
+
+
+
+
+
+
+
 }
